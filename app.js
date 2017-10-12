8ab82c9378284c4740210888a9ff932a15cf2885
yu <yuchiu2002@hotmail.com>
2017-10-12 13:22:59 -0400

    adding to github

---
+++ app.js
@@ -0,0 +1,16 @@
+$('#quoteBtn').click(() => {
+    changeQuote();
+})
+
+function changeQuote() {
+    const jokeApi = "http://api.icndb.com/jokes/random";
+    $.get(jokeApi, (data) => {
+        $('#quote').html(data["value"]["joke"]);
+    });
+}
+
+$('#tweetBtn').bind('click', function() {
+    const twitterLink = "https://twitter.com/intent/tweet?hashtags=quotes&text"
+    let quote = $('#quote').innerHTML();
+    $('#tweetBtn').attr("href", twitterLink + encodeURIComponent(quote));
+});
